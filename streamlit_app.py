c9b8747013939ce93ec3b0168864796212c6e3b0
Sk21Snow <108274283+Sk21Snow@users.noreply.github.com>
2022-06-27 13:12:39 +0100

    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,10 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
-
+   
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale, Spinach & quinoa')
-streamlit.text('hard boiled egg')
+streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
+streamlit.text('🥗 Kale, Spinach & quinoa')
+streamlit.text('🐔 hard boiled egg')
+streamlit.text('🥑🍞 avocado toast')
 
