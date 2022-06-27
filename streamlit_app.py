b6dae8d67522d0ff775d81e5c88d01c17ae4cdff
Sk21Snow <108274283+Sk21Snow@users.noreply.github.com>
2022-06-27 13:09:22 +0100

    Update streamlit_app.py
    update the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+stremalit.header('Breakfast Menu')
+stremalit.text('Omega 3 & Blueberry oatmeal')
+stremalit.text('Kale, Spinach & quinoa')
+stremalit.text('hard boiled egg')
+
