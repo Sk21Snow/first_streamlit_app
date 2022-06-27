ef66b0a5a5a2750173bf6f5fa84ff5c6734455c3
Sk21Snow <108274283+Sk21Snow@users.noreply.github.com>
2022-06-27 13:10:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
-stremalit.header('Breakfast Menu')
-stremalit.text('Omega 3 & Blueberry oatmeal')
-stremalit.text('Kale, Spinach & quinoa')
-stremalit.text('hard boiled egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale, Spinach & quinoa')
+streamlit.text('hard boiled egg')
 
